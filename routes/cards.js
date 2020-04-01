aa1679804fa476c00b7db6b7e65b8d0865e16c58
Lucas <lucas_ramos1994@hotmail.com>
2020-04-01 20:39:05 -0300

    add card Put method

--- routes/cards.js
+++ routes/cards.js
@@ -19,13 +19,6 @@ router.get('/', (req, res, next) => {
 router.post('/', (req, res, next) => {
   const Card = db.Mongoose.model('cards', db.CardSchema, 'cards');
 
-  console.log({
-    name: req.body.name,
-    attack: req.body.attack,
-    life: req.body.life,
-    defense: req.body.defense,
-  });
-
   const newcart = new Card({
     name: req.body.name,
     attack: req.body.attack,
@@ -42,6 +35,26 @@ router.post('/', (req, res, next) => {
     res.json(newcart);
     res.end();
   });
+});
+
+/* Create card. */
+router.put('/:id', (req, res, next) => {
+  const Card = db.Mongoose.model('cards', db.CardSchema, 'cards');
+
+  Card.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { upsert: true },
+    (err, doc) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        res.end();
+        return;
+      }
+      res.json(req.body);
+      res.end();
+    }
+  );
 });
 
 module.exports = router;
